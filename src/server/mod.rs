502e9af8239e7cf69e65ee76e526c8f4b3798c64
Amadeu Marques <maramadeu@gmail.com>
2023-12-04 12:37:30 +0000

    increase period

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1283,7 +1283,7 @@ impl NetworkView for MockView {
 /// Every `PERIOD` messages, the message log is cleared,
 /// and a new log checkpoint is initiated.
 /// TODO: Move this to an env variable as it can be highly dependent on the service implemented on top of it
-pub const CHECKPOINT_PERIOD: u32 = 1000;
+pub const CHECKPOINT_PERIOD: u32 = 50000;
 
 #[derive(Error, Debug)]
 pub enum SMRReplicaError {
