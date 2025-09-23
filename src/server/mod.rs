9a37e4eb5d99e974251835b0457c15e03ab849b0
Amadeu Marques <maramadeu@gmail.com>
2025-09-23 19:14:34 +0100

    decrease checkpoint

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1282,7 +1282,7 @@ impl NetworkView for MockView {
 /// TODO: Move this to an env variable as it can be highly dependent on the service implemented on top of it
 /// 
 
-pub const CHECKPOINT_PERIOD: u32 = 45000;
+pub const CHECKPOINT_PERIOD: u32 = 30000;
 
 #[derive(Error, Debug)]
 pub enum SMRReplicaError {
