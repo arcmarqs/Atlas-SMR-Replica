16dafd991369ea42405f89c3371ab8b9315f185f
Amadeu Marques <amadeu.marques@adyta.pt>
2025-09-29 12:05:07 +0000

    increase channel

--- src/server/state_transfer.rs
+++ src/server/state_transfer.rs
@@ -12,7 +12,7 @@ use atlas_core::state_transfer::{StateTransferProtocol, STMsg, STPollResult, STR
 use atlas_core::state_transfer::networking::serialize::StateTransferMessage;
 use atlas_core::timeouts::RqTimeout;
 
-pub const WORK_CHANNEL_SIZE: usize = 64;
+pub const WORK_CHANNEL_SIZE: usize = 256;
 pub const RESPONSE_CHANNEL_SIZE: usize = 64;
 
 /// A state transfer work message
