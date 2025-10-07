3e60a3c742c96fbbc11c009a0d73e10d0cd23e3a
Amadeu Marques <maramadeu@gmail.com>
2025-10-07 16:31:26 +0100

    increase checkpoint period

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1732,7 +1732,7 @@ impl NetworkView for MockView {
 /// TODO: Move this to an env variable as it can be highly dependent on the service implemented on top of it
 ///
 
-pub const CHECKPOINT_PERIOD: u32 = 50000;
+pub const CHECKPOINT_PERIOD: u32 = 100000;
 
 #[derive(Error, Debug)]
 pub enum SMRReplicaError {
